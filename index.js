2f238c8f109600b94287e62d1bdd34ee7e68f433
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-29 02:37:20 +0530

    formatting document

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express,{urlencoded} from "express";
+import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -11,35 +11,30 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-
-
-app.get("/",(req,res)=>{
-    return res.status(200).json({
-        message : "i am coming from backend",
-        success : true
-    })
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "i am coming from backend",
+    success: true,
+  });
+});
 
 //middleware
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(urlencoded({extended : true}));
+app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials : true
-}
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 app.use(cors(corsOptions));
-
 
 //yaha par api  call karna hai
 
-app.use("/api/v1/user",userRoute);
-"http://localhost:8000/api/v1/user"
+app.use("/api/v1/user", userRoute);
+("http://localhost:8000/api/v1/user");
 
-
-
-app.listen(port,() =>{
-    connectDB();
-    console.log(`server listening at port : ${port}`);
-})
+app.listen(port, () => {
+  connectDB();
+  console.log(`server listening at port : ${port}`);
+});
