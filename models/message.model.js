0ab62a1261f1ac1ef97b4d7b88f0a0739b998fc7
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-29 02:34:52 +0530

    formatting document

--- models/message.model.js
+++ models/message.model.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-    senderId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-    },
-    receiverId : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-    },
-    message : {
-        type : String,
-        required : true,
-    },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
 
-})
-
-export default Message =  mongoose.model('Message', messageSchema);
+export default Message = mongoose.model("Message", messageSchema);
