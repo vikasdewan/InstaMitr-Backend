d81d6d47fb01ef0efda569e374b008c4303c1ce1
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-29 02:34:42 +0530

    formatting document

--- models/conversation.model.js
+++ models/conversation.model.js
@@ -11,8 +11,11 @@ const conversationSchema = new mongoose.Schema({
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Message",
-    }
+    },
   ],
 });
 
-export default Conversation = mongoose.model("Conversation", conversationSchema);
+export default Conversation = mongoose.model(
+  "Conversation",
+  conversationSchema
+);
