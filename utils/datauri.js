7191c7ccb7cebf01a52f6bee88179358c783c816
VikasDewangan <dewanganvikas192@gmail.com>
2024-10-29 02:36:29 +0530

    formatting document

--- utils/datauri.js
+++ utils/datauri.js
@@ -1,13 +1,12 @@
-import  DataUriParser from 'datauri/parser.js';
-import path from 'path';
-
+import DataUriParser from "datauri/parser.js";
+import path from "path";
 
 const parser = new DataUriParser();
 
-const getDataUri = (file)=>{
-    const extName = path.extname(file.originalname).toString();
+const getDataUri = (file) => {
+  const extName = path.extname(file.originalname).toString();
 
-    return parser.format(extName,file.buffer).content;
-}
+  return parser.format(extName, file.buffer).content;
+};
 
-export default getDataUri; 
+export default getDataUri;
